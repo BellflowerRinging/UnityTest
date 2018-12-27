1ee451d47d6e58b46a7dc68cc4f3f57436f9850d
BellflowerRinging <1260188684@qq.com>
2018-12-28 00:20:37 +0800

    Lightning

---
+++ Assets/Lightning/Lightning.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Transform m_Star;
+    public Transform m_End;
+    public LineRenderer m_Line;
+    public int NodeCount;
+    public float MaxOffset;
+	[Range(0,1)]
+    public float NoieSpeed = 0.2f;
+
+
+    Vector3[] NodePostions;
+    void Start()
+    {
+        NodePostions = InitLine(m_Line, m_Star.position, m_End.position, NodeCount);
+        RandomPostions(m_Line, NodePostions, MaxOffset);
+    }
+
+    float loctime = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (loctime >= NoieSpeed)
+        {
+            RandomPostions(m_Line, NodePostions, MaxOffset);
+            loctime = 0;
+        }
+        else
+        {
+            loctime += Time.deltaTime;
+        }
+    }
+
+    Vector3[] InitLine(LineRenderer line, Vector3 star, Vector3 end, int nodeCount)
+    {
+        Vector3[] postions = new Vector3[nodeCount];
+        line.positionCount = nodeCount;
+        for (int i = 0; i < postions.Length; i++)
+        {
+            postions[i] = Vector3.Lerp(star, end, (float)i / (float)(nodeCount - 1));
+        }
+        line.SetPositions(postions);
+        return postions;
+    }
+
+    void RandomPostions(LineRenderer line, Vector3[] nodePostions, float maxOffset)
+    {
+        Vector3[] postions = new Vector3[nodePostions.Length];
+        Array.Copy(nodePostions, postions, nodePostions.Length);
+        for (int i = 1; i < line.positionCount - 1; i++)
+        {
+            postions[i] += RandomOffect(maxOffset);
+        }
+        line.SetPositions(postions);
+    }
+
+    Vector3 RandomOffect(float maxOffset)
+    {
+        return new Vector3(UnityEngine.Random.Range(-maxOffset, maxOffset), UnityEngine.Random.Range(-maxOffset, maxOffset), UnityEngine.Random.Range(-maxOffset, maxOffset));
+    }
+
+}
---
+++ Assets/Lightning/LightningUnlitShader.shader
@@ -0,0 +1,74 @@
+﻿Shader "Unlit/GradientColor"
+{
+	Properties
+	{
+		_MainTex ("Texture",2D) = "white" {}
+		_StartColor("StartColor",Color) = (0,0,0,0)
+		_EndColor("EndColor",Color) = (0,0,0,0)
+		_Offset("Min value",Range(-0.5,0.5))=1
+		_AllhaMax("Min value",Range(0,1))=1
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+		Cull off
+		Pass
+		{
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			fixed4 _StartColor;
+			fixed4 _EndColor;
+			float _Offset;
+			float _AllhaMax;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				fixed locUv=distance(i.uv.y,0.5);
+				fixed locUvFcolor=locUv+_Offset;
+				col.rgb=lerp(_StartColor.rgb,_EndColor.rgb,locUvFcolor/0.5);
+
+				col.a=clamp(lerp(1,0,locUv/_AllhaMax),0,1);
+
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
