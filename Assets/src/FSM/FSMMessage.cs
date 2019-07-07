b6895a44871c51f46b55e1c69c8d4995e7a25c05
BellflowerRinging <1260188684@qq.com>
2019-07-07 22:42:33 +0800

    FSM

---
+++ Assets/src/FSM/FSMMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public struct FSMStateMessage : IMessage
+{
+    public System.Type LastStateType;
+    public System.Type CurStateType;
+
+    public bool isEmpty()
+    {
+        return false;
+    }
+}
+
+public struct FSMStateSwitchMessage : IMessage
+{
+    public System.Type LastStateType;
+    public System.Type CurStateType;
+
+    public bool isEmpty()
+    {
+        return false;
+    }
+}
---
+++ Assets/src/FSM/IFSM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public interface IFSMState
+{
+    void Enter(FSMStateMessage message);
+    void Running(FSMStateMessage message);
+    void Exit(FSMStateMessage message);
+}
+
+public interface IAiFsmState : IFSMState
+{
+
+}
+
+public interface IFSMManager
+{
+    Dictionary<System.Type, IFSMState> InitState();
+    System.Type GetDefaultStateType();
+}
+
+public interface IFSMContorl
+{
+    IFSMState Cur_state { get; set; }
+
+    void AddListenerToSwitch(Action<FSMStateSwitchMessage> action);
+
+    void RemoveListenerToSwitch(Action<FSMStateSwitchMessage> action);
+
+    void SwitchTo(System.Type state);
+
+    void Running();
+}
